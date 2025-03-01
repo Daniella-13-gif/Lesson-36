556f849071497cc27ef1d8965a378aee1ba120d8
Daniella-13-gif <ngozihomestore@gmail.com>
2025-03-01 13:31:21 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+import getpass
+
+name = input("Enter your name") 
+  age = input("Enter your age")
+    gender = input("Enter your gender") 
+      grade = input("Enter your grade")
+      is_student = input("Are you a student? (true/false):")
+        height = input("Enter your height")
+          weight = input("Enter your weight")    
